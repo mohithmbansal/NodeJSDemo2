7c732e2c9b5df97f38260e15cee1f23492d8fd63
mohithmbansal <mohithmbansal@gmail.com>
2022-06-10 08:10:09 +0800

    Ok

---
+++ cf-nodejs-demo/app/index.js
@@ -0,0 +1,14 @@
+console.log("Test")
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello world! Cloud Foudnry Node Js Demo");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log("app listening at port " + port);
+});
